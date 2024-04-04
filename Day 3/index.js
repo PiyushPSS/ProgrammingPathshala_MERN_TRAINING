5672d4aeefbb81ba6133f96f57ed6b10a753a5a5
Piyush <piyushpratapsingh67@gmail.com>
2024-04-04 16:29:57 +0530

    Day 3 - Second Half

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,70 +1,122 @@
-const a = "message";
+// const a = "message";
 
-console.log(a);
+// console.log(a);
 
-// JavaScript Variables can be declared in 4 ways:
+// // JavaScript Variables can be declared in 4 ways:
 
-// Automatically. Scope is function level. Can be defined multiple times.
+// // Automatically. Scope is function level. Can be defined multiple times.
 
-b = 19;
+// b = 19;
 
-// Using var. Oldest keyword. Function scope. Can be defined multiple times.
+// // Using var. Oldest keyword. Function scope. Can be defined multiple times.
 
-var c = "Hello";
-var d = 10;
+// var c = "Hello";
+// var d = 10;
 
-// Using let. Let has a block level scope. Cannot be defined multiple times.
+// // Using let. Let has a block level scope. Cannot be defined multiple times.
 
-let z = 100;
+// let z = 100;
 
-// Using const
+// // Using const
 
-const j = "Constant";
+// const j = "Constant";
 
-function en() {
-    for(i = 0; i < 5 ; i++) {
-        console.log(i);
-    }
+// function en() {
+//     for(i = 0; i < 5 ; i++) {
+//         console.log(i);
+//     }
+// }
+
+// en();
+
+// //Undefined vs null.
+// //Parseint vs number.
+
+// //array.
+
+// let str = "12";
+// let age = 12;
+
+// // == and === in JS
+
+// if(str == age) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+
+// if(str === age) {
+//     console.log("YES");
+// } else {
+//     console.log("NO");
+// }
+
+
+// const names = "Piyush";
+// const filler = "My name is";
+
+// const result = `${filler} ${names}`;
+// console.log(result);
+
+// function print(str) {
+//     if(!str) {
+//         console.log("str is undefined");
+//     }
+
+//     return str;
+// }
+
+// print();
+
+//function declaration
+function print() {
+    console.log("declaration");
 }
 
-en();
+//function assignment {named} 
 
-//Undefined vs null.
-//Parseint vs number.
-
-//array.
-
-let str = "12";
-let age = 12;
-
-// == and === in JS
-
-if(str == age) {
-    console.log("YES");
-} else {
-    console.log("NO");
-}
-
-if(str === age) {
-    console.log("YES");
-} else {
-    console.log("NO");
+const a = function print() {
+    console.log("--");
 }
 
 
-const names = "Piyush";
-const filler = "My name is";
+//function assignment {unnamed} 
 
-const result = `${filler} ${names}`;
-console.log(result);
-
-function print(str) {
-    if(!str) {
-        console.log("str is undefined");
-    }
-
-    return str;
+const b = function () {
+    console.log("--");
 }
 
-print();
+///Using arrow notation in function
 
+const c = () => {
+    console.log("--");
+}
+
+//Different methods of initialization of objects.
+
+// const obj = new Object();
+// obj[1] = '1';
+// obj[2] = '2';
+// obj[3] = '13';
+
+// console.log(obj);
+
+// const obj = {};
+// obj[1] = '1';
+// obj[2] = '2';
+// obj[3] = '13';
+
+// console.log(obj);
+
+
+const obj = {
+    1: '1h',
+    2: '2h'
+};
+
+
+// console.log(obj[1]);
+
+const value = prompt("Enter the value");
+
+console.log(obj[value]);
