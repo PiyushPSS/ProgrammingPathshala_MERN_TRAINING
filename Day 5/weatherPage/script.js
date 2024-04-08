ea42cf880a7552ba9341e0934667e96e52f8fad0
Piyush <piyushpratapsingh67@gmail.com>
2024-04-08 23:03:50 +0530

    Weather Page Added

--- Day 5/weatherPage/script.js
+++ Day 5/weatherPage/script.js
@@ -1,6 +1,4 @@
-console.log("JEL")
-
-const API = "Y736T2TCB8JH5EWDRS4YRG4XG";
+const API = "9D33JTBCE2NC2PEXETJ9PA93V";
 
 function fetchDataFromAPI() {
     fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/India?unitGroup=us&key=" + API).then(res => res.json()).then(data => {
@@ -11,43 +9,50 @@ function fetchDataFromAPI() {
 fetchDataFromAPI();
 
 function getDataToUI(data) {
-    // const table = document.getElementById('root');
-    // table.style.border = "1px solid black";
-    // const tableHeader1 = document.createElement('th');
-    // const tableHeader2 = document.createElement('th');
-    // const tableHeader3 = document.createElement('th');
-    // const tablerow1 = document.createElement('tr');
-    // const tablerow2 = document.createElement('tr');
-    // tableHeader1.style.border = "1px solid black";
-    // tableHeader2.style.border = "1px solid black";
-    // tableHeader3.style.border = "1px solid black";
-
-    // tableHeader1.innerText = "Header1";
-    // tableHeader2.innerText = "Header2";
-    // tableHeader3.innerText = "Header3";
-
-    // const tableData1 = document.createElement('td');
-    // const tableData2 = document.createElement('td');
-    // const tableData3 = document.createElement('td');
-
-    // tableData1.innerText = "Data1"
-    // tableData2.innerText = "Data2"
-    // tableData3.innerText = "Data3"
-
-    // tablerow1.appendChild(tableHeader1);
-    // tablerow1.appendChild(tableHeader2);
-    // tablerow1.appendChild(tableHeader3);
-    // tablerow2.appendChild(tableData1);
-    // tablerow2.appendChild(tableData2);
-    // tablerow2.appendChild(tableData3);
-    // table.appendChild(tablerow1);
-    // table.appendChild(tablerow2);
 
     const days = data.days;
+    const table = document.getElementById('root');
+    table.style.border = "1px solid black";
 
-    for(i = 0 ; i < days.length ; i++) {
-        console.log(i);
+
+    const dateTime = document.createElement('th');
+    const tempMax = document.createElement('th');
+    const tempMin = document.createElement('th');
+    const humidity = document.createElement('th');
+    const tablerow1 = document.createElement('tr');
+
+    dateTime.innerText = "Date";
+    tempMax.innerText = "Maximum Temperature";
+    tempMin.innerText = "Minimum Temperature";
+    humidity.innerText = "Humidity";
+
+
+    tablerow1.appendChild(dateTime);
+    tablerow1.appendChild(tempMax);
+    tablerow1.appendChild(tempMin);
+    tablerow1.appendChild(humidity);
+    table.appendChild(tablerow1);
+
+
+    for (i = 0; i < days.length; i++) {
+        const singleDay = days[i];
+
+        const dateTime_data = document.createElement('td');
+        const tempMax_data = document.createElement('td');
+        const tempMin_data = document.createElement('td');
+        const humidity_data = document.createElement('td');
+        const tablerow_data = document.createElement('tr');
+
+        dateTime_data.innerText = singleDay.datetime;
+        tempMax_data.innerText = singleDay.tempmax;
+        tempMin_data.innerText = singleDay.tempmin;
+        humidity_data.innerText = singleDay.humidity;
+
+
+        tablerow_data.appendChild(dateTime_data);
+        tablerow_data.appendChild(tempMax_data);
+        tablerow_data.appendChild(tempMin_data);
+        tablerow_data.appendChild(humidity_data);
+        table.appendChild(tablerow_data);
     }
 }
-
-// getDataToUI();
